541badfd5ea6770f1fefb271a15ac40130830256
David Shore <david@foggymedia.com>
2024-01-18 13:23:05 +0100

    v3 updating state

---
+++ v3/updating_state/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import StateAsSnapshot3 from "./StateAsSnapshot3";
+import MovingDot from "./MovingDot";
+import Form from "./Form";
+
+function App() {
+  return (
+    <>
+      <Form />
+    </>
+  );
+}
+
+export default App;
