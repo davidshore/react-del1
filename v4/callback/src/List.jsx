5871911300ff67bae4eb8cecb7bc96187837a143
David Shore <david@foggymedia.com>
2024-01-24 11:01:26 +0100

    v4

--- v4/callback/src/List.jsx
+++ v4/callback/src/List.jsx
@@ -1,10 +1,20 @@
+import Item from "./Item";
+
+// title: "React",
+//     url: "https://reactjs.org/",
+//     author: "Jordan Walke",
+//     num_comments: 3,
+//     points: 4,
+
+//     objectID: 0,
+
 export default function List(props) {
   const list = props.list;
 
   return (
     <ul>
-      {list.map((item) => {
-        return <li key={item.objectID}>{item.title}</li>;
+      {list.map(({ objectID, ...item }) => {
+        return <Item key={objectID} {...item} />;
       })}
     </ul>
   );
