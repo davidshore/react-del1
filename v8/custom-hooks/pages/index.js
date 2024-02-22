5f1c6e847eb6ede60fc2798d09aa596ad7d09162
David Shore <david@foggymedia.com>
2024-02-22 10:06:33 +0100

    custom hooks

---
+++ v8/custom-hooks/pages/index.js
@@ -0,0 +1,18 @@
+import useOnlineStatus from "@/hooks/useOnlineStatus";
+
+export default function Home() {
+  const isOnline = useOnlineStatus();
+
+  return (
+    <main>
+      <h1>Sida 1</h1>
+      <h2>
+        {isOnline ? (
+          <p className="text-green-500">Du är online</p>
+        ) : (
+          <p className="text-red-500">Du är offline</p>
+        )}
+      </h2>
+    </main>
+  );
+}
